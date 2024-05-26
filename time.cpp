68be70dbd4aebefd09af19e28db0f24c7f5f295e
aireater <aireater514@gmail.com>
2024-05-26 17:58:25 +0900

    ver 0.0.3 てすと？

---
+++ time.cpp
@@ -0,0 +1,40 @@
+#include <chrono>
+
+#include "CNN/out.hpp"
+
+constexpr int COUNT = 10;
+
+// ベンチマーク用のテンプレート関数
+template <typename Func>
+void benchmark(Func func, const std::string &funcName)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    // 対象関数を実行
+    for (int i = 0; i < COUNT; ++i)
+    {
+        func();
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration = end - start;
+
+    std::cout << "Execution time for " << funcName << ": " << duration.count() << " ms" << std::endl;
+}
+
+// ベンチマーク対象の関数
+void testA()
+{
+}
+
+void testB()
+{
+}
+
+int main()
+{
+    benchmark(testA, "test-A");
+    benchmark(testB, "test-B");
+
+    return 0;
+}
